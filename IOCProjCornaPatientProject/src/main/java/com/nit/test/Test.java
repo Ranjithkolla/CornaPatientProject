67a80af395f310116f64aacde0ab6d28e97cff13
Ranjith kolla TL <ranjithreddykolla@gmail.com>
2021-06-17 12:45:23 +0530

    add m1 method

--- IOCProjCornaPatientProject/src/main/java/com/nit/test/Test.java
+++ IOCProjCornaPatientProject/src/main/java/com/nit/test/Test.java
@@ -2,6 +2,11 @@ package com.nit.test;
 
 public class Test {
 
+	
+	public void m1()
+	{
+		System.out.println("Test.m1()");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
